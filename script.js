08947948e2f920df6f8654e47d1a9a25c0484aa5
Telcean Marius-Andrei <80281030+mariustelcean96@users.noreply.github.com>
2022-04-09 13:55:42 +0300

    Add -, *, / and operate functions

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function sum(x, y) {
   return x + y;
 }
+function subtract(x, y) {
+  return x - y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function divide(x, y) {
+  return x / y;
+}
+function operate(operator, x, y) {
+  if (operator === '+') {sum(x,y)}
+  else if (operator === '-') {subtract(x,y)}
+  else if (operator === '*') {multiply(x,y)}
+  else if (operator === '/') {divide(x,y)}
+  else {
+  return false;
+  }
+}
