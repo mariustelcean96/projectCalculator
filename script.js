1951f9fb6f8c7736d59ae9f5e4a34c29a46f7089
Telcean Marius-Andrei <80281030+mariustelcean96@users.noreply.github.com>
2022-04-10 13:36:22 +0300

    Add events for buttons

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-function sum(x, y) {
-  return x + y;
+const resultsDiv = document.querySelector('.output'); 
+let displayValueNumbers = [];
+let operator;
+let var1 = 0;
+let var2 = 0;
+
+ function sum(x, y) {
+  resultsDiv.textContent = x + y;
 }
+
 function subtract(x, y) {
-  return x - y;
+  resultsDiv.textContent = x - y;
 }
+
 function multiply(x, y) {
-  return x * y;
+  resultsDiv.textContent = x * y;
 }
+
 function divide(x, y) {
-  return x / y;
+  resultsDiv.textContent = x / y;
 }
+
 function operate(operator, x, y) {
   if (operator === '+') {sum(x,y)}
   else if (operator === '-') {subtract(x,y)}
@@ -19,3 +29,31 @@ function operate(operator, x, y) {
   return false;
   }
 }
+
+const numbersNodeList = document.querySelectorAll('.number');
+numbersNodeList.forEach((button) => {
+  button.addEventListener('click', () => {
+  resultsDiv.textContent += button.value;
+  });
+});
+
+const operatorsNodeList = document.querySelectorAll('.operator');
+operatorsNodeList.forEach((button) => {
+  button.addEventListener('click', () => {
+  var1 = (Number(resultsDiv.textContent));
+  resultsDiv.textContent = '';
+  operator = button.value;
+  });
+});
+
+const equalSign = document.querySelector('.result');
+equalSign.addEventListener('click', () => {
+  var2 = Number(resultsDiv.textContent);
+  operate(operator, var1, var2);
+});
+
+const clearSign = document.querySelector('.clear');
+clearSign.addEventListener('click', () => {
+  var1 = var2 = 0;
+  resultsDiv.textContent = '';
+});
